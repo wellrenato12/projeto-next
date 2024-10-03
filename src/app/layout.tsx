9b04f6e8634fbd5e125c12fc50151347cb8c2446
Wellington Albino <well.renato@hotmail.com>
2024-10-03 12:35:58 -0300

    Conceitos sobre layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header>Cabeçalho da aplicação</header>
         {children}
+        <footer>Rodapé da aplicação</footer>
       </body>
     </html>
   );
